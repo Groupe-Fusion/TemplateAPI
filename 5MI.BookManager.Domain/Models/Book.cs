f82915459fda8ede27cc8eb7126744a0aa6dc364
Snoopyyyyy <kentincosnard@gmail.com>
2025-01-17 14:14:36 +0100

    setup domain

---
+++ 5MI.BookManager.Domain/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace _5MI.BookManager.Domain.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsBorrowed { get; set; }
+    }
+}
---
+++ 5MI.BookManager.Domain/Repositories.Core/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using _5MI.BookManager.Domain.Models;
+
+namespace _5MI.BookManager.Domain.Repositories.Core
+{
+    public interface IBookRepository
+    {
+        Task<List<Book>> GetAllBooksAsync(CancellationToken ct = default);
+        Task<Book?> GetBookByIdAsync(int id,  CancellationToken ct = default);
+        Task<Book> AddBookAsync(Book book, CancellationToken ct = default);
+        Task<Book> UpdateBookAsync(Book book, CancellationToken ct = default);
+        Task<Book> DeleteBookAsync(int id, CancellationToken ct = default);
+    }
+}
