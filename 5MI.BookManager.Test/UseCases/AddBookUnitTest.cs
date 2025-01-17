03c8a50ef6952c69357e4e60a0e33f9f41faa14a
Snoopyyyyy <kentincosnard@gmail.com>
2025-01-17 15:18:43 +0100

    add test for usecase

---
+++ 5MI.BookManager.Test/UseCases/AddBookUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using _5MI.BookManager.Applicatif.UseCases;
+using _5MI.BookManager.Domain.Models;
+using _5MI.BookManager.Domain.Repositories.Core;
+using AutoFixture;
+using Moq;
+
+namespace _5MI.BookManager.Test.UseCases
+{
+    public class AddBookUnitTest
+    {
+        [Fact]
+        public async void Success()
+        {
+            // créer les fictures
+            var fixture = new Fixture();
+            var book = fixture.Create<Book>();
+
+            // setup le mock pour simuler le repository
+            var bookrepositoryMoq = new Mock<IBookRepository>();
+            bookrepositoryMoq
+                .Setup(x => x.AddBookAsync(It.IsAny<Book>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(book);
+
+            // Test du usecase
+            var addBookUseCase = new AddBookUseCase(bookrepositoryMoq.Object);
+            var newBook = await addBookUseCase.ExecuteAsync(book);
+
+            // Verification
+            Assert.Equal(book.Id, newBook.Id);
+        }
+
+        [Fact]
+        public async void No_Title_Set()
+        {
+            // crash car pas de titre
+        }
+    }
+}
