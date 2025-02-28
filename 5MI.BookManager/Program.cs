65a38f94f482eceba08c4ad123a7733cbc6a8595
Snoopy <kentincosnard@gmail.com>
2025-02-28 13:57:26 +0100

    fix auto migrate

--- 5MI.BookManager/Program.cs
+++ 5MI.BookManager/Program.cs
@@ -54,6 +54,6 @@ app.MapControllers();
 // auto migrate database
 using var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetService<BookManagerContext>()!;
-context.Database.Migrate();
+//context.Database.Migrate();
 
 app.Run();
