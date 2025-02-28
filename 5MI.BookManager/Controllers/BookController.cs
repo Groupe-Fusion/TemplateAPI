0e87182189338cd890221dc8b77a26bd74e944e2
Maelys Ami <maelys.ami@agence-dbcom.fr>
2025-02-28 12:09:09 +0100

    Add update and delete in swagger

--- 5MI.BookManager/Controllers/BookController.cs
+++ 5MI.BookManager/Controllers/BookController.cs
@@ -4,6 +4,7 @@ using _5MI.BookManager.Domain.Models;
 using _5MI.BookManager.DTO;
 using _5MI.BookManager.Mapper;
 using _5MI.BookManager.DTO.Requests;
+using _5MI.BookManager.Applicatif.UseCases;
 
 namespace _5MI.BookManager.Presentation.Controllers
 {
@@ -14,6 +15,8 @@ namespace _5MI.BookManager.Presentation.Controllers
         private readonly IAddBookUseCase _addBookUseCase;
         private readonly IGetAllBooksUseCase _getAllBooksUseCase;
         private readonly IGetBookByIdUseCase _getBookByIdUseCase;
+        private readonly IEditBookUseCase _editBookUseCase;
+        private readonly IDeleteBookUseCase _deleteBookUseCase;
 
         public BorrowBookController(
             IAddBookUseCase addBookUseCase,
@@ -42,6 +45,55 @@ namespace _5MI.BookManager.Presentation.Controllers
                 return StatusCode(500, $"errror {ex.Message}");
             }
         }
+
+        /// <summary>
+        /// Modifier un livre existant.
+        /// </summary>
+        [HttpPut("{bookId}")]
+        public async Task<IActionResult> UpdateBook(int bookId, [FromBody] BookRequest bookRequest, CancellationToken ct)
+        {
+            try
+            {
+                var updatedBook = BookMapper.ToBookEntity(bookRequest);
+                updatedBook.Id = bookId;
+
+                var book = await _editBookUseCase.ExecuteAsync(updatedBook, ct);
+
+                return Ok(book);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound($"Book with ID {bookId} not found");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error {ex.Message}");
+            }
+        }
+
+
+        /// <summary>
+        /// Supprimer un livre par son ID.
+        /// </summary>
+        [HttpDelete("{bookId}")]
+        public async Task<IActionResult> DeleteBook(int bookId, CancellationToken ct)
+        {
+            try
+            {
+                await _deleteBookUseCase.ExecuteAsync(bookId, ct);
+
+                return NoContent();
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound($"Book with ID {bookId} not found");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error {ex.Message}");
+            }
+        }
+
 
         /// <summary>
         /// Récupérer tous les livres.
