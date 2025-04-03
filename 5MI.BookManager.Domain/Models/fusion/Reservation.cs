f41a0fabaa0ef5e107313e018db920389f870247
r.germain <r.germain@e-maritime.fr>
2025-04-03 11:14:46 +0200

    add domain reservation

---
+++ 5MI.BookManager.Domain/Models/fusion/Reservation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5MI.BookManager.Domain.Models.fusion
+{
+    class Reservation
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int CreatedBy { get; set; }
+        public string ReservationTitle { get; set; } = string.Empty;
+        public ReservationType Type { get; set; }
+        public virtual User User { get; set; }
+    }
+}
